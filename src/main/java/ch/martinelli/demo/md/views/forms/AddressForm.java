f53b648ecf320de2be64d0561985e8f3bbb91cd9
Simon Martinelli <simon@72.services>
2023-07-14 16:13:29 +0200

    Merged forms

--- src/main/java/ch/martinelli/demo/md/views/forms/AddressForm.java
+++ src/main/java/ch/martinelli/demo/md/views/forms/AddressForm.java
@@ -1,16 +1,11 @@
 package ch.martinelli.demo.md.views.forms;
 
 import ch.martinelli.demo.md.data.entity.Address;
-import ch.martinelli.demo.md.data.service.AddressService;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 
@@ -22,28 +17,17 @@ public class AddressForm extends Div {
     private final ComboBox<String> state = new ComboBox<>("State");
     private final ComboBox<String> country = new ComboBox<>("Country");
 
-    private final Button cancel = new Button("Cancel");
-    private final Button save = new Button("Save");
-
     private final Binder<Address> binder = new Binder<>(Address.class);
 
-    public AddressForm(AddressService addressService) {
+    public AddressForm() {
         addClassName("address-form-view");
 
         add(createTitle());
         add(createFormLayout());
-        add(createButtonLayout());
 
         binder.bindInstanceFields(this);
 
         clearForm();
-
-        cancel.addClickListener(e -> clearForm());
-        save.addClickListener(e -> {
-            addressService.update(binder.getBean());
-            Notification.show(binder.getBean().getClass().getSimpleName() + " stored.");
-            clearForm();
-        });
     }
 
     private Component createTitle() {
@@ -60,17 +44,11 @@ public class AddressForm extends Div {
         return formLayout;
     }
 
-    private Component createButtonLayout() {
-        HorizontalLayout buttonLayout = new HorizontalLayout();
-        buttonLayout.addClassName("button-layout");
-        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        buttonLayout.add(save);
-        buttonLayout.add(cancel);
-        return buttonLayout;
+    public void clearForm() {
+        binder.setBean(new Address());
     }
 
-    private void clearForm() {
-        this.binder.setBean(new Address());
+    public Address getBean() {
+        return binder.getBean();
     }
-
 }
--- src/main/java/ch/martinelli/demo/md/views/forms/PersonForm.java
+++ src/main/java/ch/martinelli/demo/md/views/forms/PersonForm.java
@@ -1,17 +1,13 @@
 package ch.martinelli.demo.md.views.forms;
 
 import ch.martinelli.demo.md.data.entity.Person;
-import ch.martinelli.demo.md.data.service.PersonService;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.customfield.CustomField;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -26,30 +22,19 @@ public class PersonForm extends Div {
     private final PhoneNumberField phone = new PhoneNumberField("Phone number");
     private final TextField occupation = new TextField("Occupation");
 
-    private final Button cancel = new Button("Cancel");
-    private final Button save = new Button("Save");
-
     private final Binder<Person> binder = new Binder<>(Person.class);
 
-    public PersonForm(PersonService personService) {
+    public PersonForm() {
         addClassName("person-form-view");
 
         add(createTitle());
         add(createFormLayout());
-        add(createButtonLayout());
 
         binder.bindInstanceFields(this);
         clearForm();
-
-        cancel.addClickListener(e -> clearForm());
-        save.addClickListener(e -> {
-            personService.update(binder.getBean());
-            Notification.show(binder.getBean().getClass().getSimpleName() + " details stored.");
-            clearForm();
-        });
     }
 
-    private void clearForm() {
+    public void clearForm() {
         binder.setBean(new Person());
     }
 
@@ -64,13 +49,8 @@ public class PersonForm extends Div {
         return formLayout;
     }
 
-    private Component createButtonLayout() {
-        HorizontalLayout buttonLayout = new HorizontalLayout();
-        buttonLayout.addClassName("button-layout");
-        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        buttonLayout.add(save);
-        buttonLayout.add(cancel);
-        return buttonLayout;
+    public Person getBean() {
+        return binder.getBean();
     }
 
     private static class PhoneNumberField extends CustomField<String> {
@@ -93,10 +73,10 @@ public class PersonForm extends Div {
         @Override
         protected String generateModelValue() {
             if (countryCode.getValue() != null && number.getValue() != null) {
-                String s = countryCode.getValue() + " " + number.getValue();
-                return s;
+                return countryCode.getValue() + " " + number.getValue();
+            } else {
+                return "";
             }
-            return "";
         }
 
         @Override
